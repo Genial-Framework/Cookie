aedc9935f824d6b728476ceb2e090926eb68b079
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 21:05:46 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -1,1 +1,32 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Cookie> For the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cookie/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Cookie;
+
+/**
+ * Utils.
+ */
+class Utils
+{
+    /**
+     * getCookieParams().
+     *
+     * Get the currently set cookie params.
+     * 
+     * @return array Return the currently set cookie params.
+     */
+    public static function getCookieParams()
+    {
+        return session_get_cookie_params();
+    }
+}
