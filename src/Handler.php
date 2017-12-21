2f1e0b6a3e16a9e3b6fc5b898cace4381e1b9ec8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-20 21:39:35 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -1,1 +1,44 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Cookie> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cookie/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Cookie;
+
+/**
+ * Handler.
+ */
+class Handler implements HandlerInterface
+{
+    public function set(string $name = null, $value = null, $expire)
+    {
+        if (is_null($name)) {
+            throw new Exception\BadMethodCallException(sprintf(
+                '`%s` The `$name` argument is missing.',
+                __METHOD__
+            ));
+        }
+        $name = trim($name);
+        if (empty($name) || $name == '') {
+            throw new Exception\UnexpectedValueException(sprintf(
+                '`%s` The `$name` argument is empty.',
+                __METHOD__
+            ));
+        }
+        $value = Utils::encode($value);
+        setcookie(
+          
+        );
+        $_COOKIE[$name] = $value;
+    }
+  
+  
+}
