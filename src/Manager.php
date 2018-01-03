524cd1afd28074c06f8427343408bda22ca325fb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-03 14:35:20 -0500

    Update Manager.php

--- src/Manager.php
+++ src/Manager.php
@@ -1,1 +1,26 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Cookie> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cookie/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Cookie;
+
+/**
+ * Manager.
+ */
+class Manager implements ManagerInterface
+{
+  
+    /**
+     * @var array|Utils::getCookieParams() $cookieParams The internal defualt cookie params.
+     */
+    private $cookieParams = Utils::getCookieParams();
+  
+}
