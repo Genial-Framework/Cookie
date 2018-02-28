28cd1865121543ad8c146f198459049bb60eb2bf
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-27 19:43:48 -0500

    Update and rename main.bootstrap.php to boot.php

---
+++ boot.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of Genial Framework.
+ *
+ * @link    <https://github.com/genial-framework/cookie> Github Repository.
+ * @license <https://github.com/genial-framework/cookie/blob/master/LICENSE> Apache-2.0
+ */
