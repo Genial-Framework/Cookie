2ab54a8f0fc1db872bc6ad053909ae06de3ec2ea
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-27 19:45:06 -0500

    Update boot.php

--- boot.php
+++ boot.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
  * This file is a part of Genial Framework.
  *
  * @link    <https://github.com/genial-framework/cookie> Github Repository.
- * @license <https://github.com/genial-framework/cookie/blob/master/LICENSE> Apache-2.0
+ * @license <https://github.com/genial-framework/cookie/blob/master/LICENSE> BSD-3-Clause
  */
